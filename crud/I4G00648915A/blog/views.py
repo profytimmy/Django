0d951910ee8b1846a629a64ecb99bbcfd82bc42b
profytimmy <oyewaletimmy01@yahoo.com>
2022-06-23 10:05:02 +0000

    pkey

--- crud/I4G00648915A/blog/views.py
+++ crud/I4G00648915A/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, ListView, DetailView, UpdateView, DeleteView
+from .models import Post
 
-# Create your views here.
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy("blog:all")
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy("blog:all")
+
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy("blog:all")
